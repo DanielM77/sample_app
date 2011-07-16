430b8b9cfc29dafce137de88f53350153208388b
Daniel Muenstermann <daniel.muenstermann@online.de>
2011-07-16 19:27:49 +0200

    Adding some Layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 SampleApp::Application.routes.draw do
+  
+  match '/signup', :to => 'users#new'
+
+  get "users/new"
+
+  match '/', :to => 'pages#home'
+  match '/home', :to => "pages#home"
+  match '/contact', :to => "pages#contact"
+  match '/about', :to => "pages#about"
+  match '/help', :to => "pages#help"
+  
   get "pages/home"
 
   get "pages/contact"
 
   get "pages/about"
+  
+  get "pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
