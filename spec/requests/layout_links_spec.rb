e85bfed9ba91d50d03001acddd130cddaece642c
Daniel Muenstermann <daniel.muenstermann@online.de>
2011-07-16 20:14:10 +0200

    Mostly testing things and a new User

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,27 +2,24 @@ require 'spec_helper'
 
 describe "LayoutLinks" do
 
-  it "should have a Home page at '/'" do
-    get '/'
-    response.should have_selector('title', :content => "Home")
-  end
-
-  it "should have a Contact page at '/contact'" do
-    get '/contact'
-    response.should have_selector('title', :content => "Contact")
-  end
-
-  it "should have an About page at '/about'" do
-    get '/about'
+  it "GET /about should return 'About' as title" do 
+    get "/about"
     response.should have_selector('title', :content => "About")
-  end
-
-  it "should have a Help page at '/help'" do
-    get '/help'
+  end    
+  it "GET /help should return 'Help' as title" do        
+    get "/help"
     response.should have_selector('title', :content => "Help")
-  end
-  it "should have a signup page at '/signup'" do
-    get '/signup'
+  end    
+  it "GET /contact should return 'Contact' as title" do      
+    get "/contact"
+    response.should have_selector('title', :content => "Contact")
+  end    
+  it "GET /home should return 'Home' as title" do  
+    get "/home"
+    response.should have_selector('title', :content => "Home")
+  end    
+  it "GET /signup should return 'Sign up' as title" do  
+    get "/signup"
     response.should have_selector('title', :content => "Sign up")
-  end  
+  end    
 end
